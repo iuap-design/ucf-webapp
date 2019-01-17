e94fc578c48d4f57374a675810c4c425b730219a
Kvkens <kvkens@qq.com>
2019-01-17 14:19:09 +0800

    拆分common样式

--- ucf-apps/demo-app-organization/src/app.js
+++ ucf-apps/demo-app-organization/src/app.js
@@ -8,6 +8,7 @@ import { ConnectedHome } from "./container";
 
 import 'ucf-common/styles/tinper-bee.css';
 import 'bee-table/build/Table.css';
+import 'ucf-common/styles/public.less';
 import './app.less';
 
 render(<ConnectedHome />, document.querySelector("#app"));
